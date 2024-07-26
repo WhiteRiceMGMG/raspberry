6bbf3ef43433291353d9f876f19c498220a36590
ogawa <ocmikanmgmg@gmail.com>
2024-07-26 15:47:21 +0900

    a

---
+++ sunfounder/monitor.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include <string.h>
+
+int LCDAddr = 0x27;
+int BLEN = 1;
+int fd;
+
+void write_word(int data){
+	int temp = data;
+	if ( BLEN == 1 )
+		temp |= 0x08;
+	else
+		temp &= 0xF7;
+	wiringPiI2CWrite(fd, temp);
+}
+
+void send_command(int comm){
+	int buf;
+	buf = comm & 0xF0;
+	buf |= 0x04;			
+	write_word(buf);
+	delay(2);
+	buf &= 0xFB;			
+	write_word(buf);
+
+	buf = (comm & 0x0F) << 4;
+	buf |= 0x04;			
+	write_word(buf);
+	delay(2);
+	buf &= 0xFB;			
+	write_word(buf);
+}
+
+void send_data(int data){
+	int buf;
+	buf = data & 0xF0;
+	buf |= 0x05;			
+	write_word(buf);
+	delay(2);
+	buf &= 0xFB;			
+	write_word(buf);
+
+	buf = (data & 0x0F) << 4;
+	buf |= 0x05;			
+	write_word(buf);
+	delay(2);
+	buf &= 0xFB;			
+	write_word(buf);
+}
+
+void init(){
+	send_command(0x33);	
+	delay(5);
+	send_command(0x32);	
+	delay(5);
+	send_command(0x28);	
+	delay(5);
+	send_command(0x0C);	
+	delay(5);
+	send_command(0x01);	
+	wiringPiI2CWrite(fd, 0x08);
+}
+
+void clear(){
+	send_command(0x01);	
+}
+
+void write(int x, int y, char data[]){
+	int addr, i;
+	int tmp;
+	if (x < 0)  x = 0;
+	if (x > 15) x = 15;
+	if (y < 0)  y = 0;
+	if (y > 1)  y = 1;
+
+	addr = 0x80 + 0x40 * y + x;
+	send_command(addr);
+	
+	tmp = strlen(data);
+	for (i = 0; i < tmp; i++){
+		send_data(data[i]);
+	}
+}
+
+
+void main(){
+	fd = wiringPiI2CSetup(LCDAddr);
+	init();
+	write(0, 0, "Yamamoto");
+	write(1, 1, "Korosu");
+}
